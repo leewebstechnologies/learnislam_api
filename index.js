e01034053ebf6e1f4a5e26ac09ac79fcdbc41bdb
leewebstechnologies <aliuajibade317@gmail.com>
2024-02-26 18:35:23 +0100

    debugging

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
-import postRoute from "./routes/posts.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use(express.json()); //express json() so as to be able to send data to our DB.
-app.use("/api/posts", postRoute);
+app.use(); //express json() so as to be able to send data to our DB.
+app.use("/api/auth".authRoutes);
+app.use("/api/users".userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(8800, () => {
   console.log("Connected to DB");
--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,4 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/learnislam", (req, res) => {
-  res.json("This is post");
-});
-
 export default router;
