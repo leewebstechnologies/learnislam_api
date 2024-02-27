cfcab57a5c2213fb7ffb37751ba2f073b192a80d
leewebstechnologies <aliuajibade317@gmail.com>
2024-02-27 10:24:00 +0100

    cookie-parser installed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import { db } from "../db.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   // CHECK EXISTING USER
@@ -23,6 +24,32 @@ export const register = (req, res) => {
   });
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  // CHECK USER
+  const q = "SELECT * FROM users WHERE username = ?";
+
+  db.query(q, [req.body.username], (error, data) => {
+    if (error) return res.json(error);
+    if (data.length === 0) return res.status(404).json("User not found!");
+
+    // CHECK PASSWORD
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!isPasswordCorrect)
+      return res.status(400).json("Wrong username or password!");
+
+    const token = jwt.sign({ id: data[0].id }, "jwtkey");
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(other);
+  });
+};
 
 export const logout = (req, res) => {};
